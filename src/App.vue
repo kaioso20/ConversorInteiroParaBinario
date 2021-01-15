d9f6194aeb7b5f1dffb15cc867448a0102de8624
Caio Rafael <caio@grafftecnologia.com.br>
2021-01-15 15:50:16 -0300

    Implementação do projeto com vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Conversor></Conversor>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conversor from './components/Conversor/Conversor'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Conversor
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Conversor/Conversor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="box">
+      <div class="taCenter pd50 divBoxShowResult">
+        <div>{{ ResultShow }}</div>
+      </div>
+      <div class="taCenter pd50 divBoxInputNumber">
+        <input
+          type="number"
+          class="taCenter inputDesign"
+          @keyup="() => ConvertNumber()"
+          v-model="NumeroInteiro"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./Conversor.css";
+export default {
+  name: "Conversor",
+  data() {
+    return {
+      NumeroInteiro: "",
+      ResultShow: "0",
+    };
+  },
+  methods: {
+    ConvertNumber() {
+      this.ResultShow = "0";
+      this.ResultShow = (parseInt(this.NumeroInteiro) >>> 0).toString(2);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
